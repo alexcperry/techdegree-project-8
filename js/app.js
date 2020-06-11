ebf2ce4544fbcb772c770451c8f9fd31538c93eb
Alexander C Perry <acp96@cornell.edu>
2020-06-11 19:51:47 -0400

    some styling, now all US users

--- js/app.js
+++ js/app.js
@@ -13,14 +13,16 @@ const makeUserDiv = (user, grid) => {
 
   userDiv.innerHTML = `
   <img src="${photo}" alt="A headshot of ${name}">
+  <div class="user-info">
   <h3>${name}</h3>
   <p>${email}</p>
   <p>${location}</p>
+  </div>
   `;
   grid.appendChild(userDiv);
 }
 
-fetch(`https://randomuser.me/api/?results=${numUsers}`)
+fetch(`https://randomuser.me/api/?nat=us&results=${numUsers}`)
   .then(response => response.json())
   .then(data => data.results)
   .then(results => results.forEach(user => makeUserDiv(user, gridDiv)));  //data.results is the list of person objects
