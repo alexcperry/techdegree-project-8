03f0dafd6d96ade7f6fe7bdc1aa60451268bcd1d
Alexander C Perry <acp96@cornell.edu>
2020-06-12 17:33:13 -0400

    scroll buttons added

--- js/app.js
+++ js/app.js
@@ -62,6 +62,7 @@ const getStateCode = stateName => {
   return states[stateName];
 }
 
+
 const birthdateToBDay = birthdate => {
   birthdate = birthdate.slice(0, 10);
   const yearAbbrev = birthdate.slice(2, 4);
@@ -70,8 +71,107 @@ const birthdateToBDay = birthdate => {
   return `${month}/${day}/${yearAbbrev}`;
 }
 
+
 //SOMETHING ELSE
-const createModalPopUp = (employee, overlay) => {
+const createModalEmployeeInfo = (index, employees) => {
+  const employee = employees[index];
+  const img = document.createElement('img');
+  img.src = employee.photo;
+  img.alt = `A headshot of ${employee.name}`;
+  img.className = "employee-photo";
+
+  const basicInfo = document.createElement('div');
+  basicInfo.className = "employee-basic-info";
+  const name = document.createElement('h3');
+  const email = document.createElement('p');
+  const city = document.createElement('p');
+  name.innerText = employee.name;
+  email.innerText = employee.email;
+  city.innerText = employee.city;
+  basicInfo.appendChild(name);
+  basicInfo.appendChild(email);
+  basicInfo.appendChild(city);
+
+  const hr = document.createElement('hr');
+
+  const extraInfo = document.createElement('div');
+  extraInfo.className = "employee-extra-info";
+  const phone = document.createElement('p');
+  const address = document.createElement('p');
+  const birthday = document.createElement('p');
+  phone.innerText = employee.phone;
+  address.innerText = employee.address;
+  birthday.innerText = employee.birthday;
+  extraInfo.appendChild(phone);
+  extraInfo.appendChild(address);
+  extraInfo.appendChild(birthday);
+
+  return {
+    'img': img,
+    'basicInfo': basicInfo,
+    'hr': hr,
+    'extraInfo': extraInfo,
+  };
+}
+
+
+const removeModalEmployeeInfo = modalDiv => {
+  const img = document.querySelector('.employee-modal img');
+  const basicInfo = document.querySelector('.employee-basic-info');
+  const hr = document.querySelector('hr');
+  const extraInfo = document.querySelector('.employee-extra-info');
+  const toRemove = [img, basicInfo, hr, extraInfo];
+
+  toRemove.forEach(elmt => modalDiv.removeChild(elmt));
+}
+
+
+const createScrollButton = (direction, currIndex, modalDiv, employees) => {
+  //Create scroll button
+  const modalScrollBtn = document.createElement('button');
+  modalScrollBtn.className = `scroll-btn`;
+  modalScrollBtn.currIndex = currIndex;
+  modalScrollBtn.innerText = (direction === "left") ? "<" : ">";
+
+  //Give button functionality
+  modalScrollBtn.addEventListener('click', () => {
+
+
+    //New Index
+    const indexChange = (direction === "left") ? (-1) : (1);
+    const proposedIndex = modalScrollBtn.currIndex + indexChange;
+    let newIndex = -1;
+    if (proposedIndex >= 0 && proposedIndex < 12) {
+      newIndex = proposedIndex;
+    } else if (proposedIndex < 0) {
+      newIndex = proposedIndex + 12;
+    } else {
+      newIndex = proposedIndex - 12;
+    }
+
+    //Change inner HTML
+    const divInnerHTML = createModalEmployeeInfo(newIndex, employees)
+    removeModalEmployeeInfo(modalDiv);
+    const firstScrollBtn = document.querySelector('.scroll-btn');
+    modalDiv.insertBefore(divInnerHTML.img, firstScrollBtn);
+    modalDiv.insertBefore(divInnerHTML.basicInfo, firstScrollBtn);
+    modalDiv.insertBefore(divInnerHTML.hr, firstScrollBtn);
+    modalDiv.insertBefore(divInnerHTML.extraInfo, firstScrollBtn);
+
+    //Update scroll buttons with index
+    const scrollBtns = document.querySelectorAll('.scroll-btn');
+    scrollBtns.forEach(btn => {
+      btn.currIndex = newIndex;
+    })
+
+  })
+
+  //Add scroll button to div
+  modalDiv.appendChild(modalScrollBtn);
+}
+
+
+const createModalPopUp = (index, employees, overlay) => {
   //Create modal div
   const modalDiv = document.createElement('div');
   modalDiv.className = "employee-modal";
@@ -88,21 +188,16 @@ const createModalPopUp = (employee, overlay) => {
   })
 
   //Inner HTML
-  modalDiv.innerHTML = `
-  <button class="end-modal-btn"><p>X</p></button>
-  <img src="${employee.photo}" alt="A headshot of ${employee.name}" class="employee-photo"">
-  <div class="employee-basic-info">
-  <h3>${employee.name}</h3>
-  <p>${employee.email}</p>
-  <p>${employee.city}</p>
-  </div>
-  <hr>
-  <div class="employee-extra-info">
-  <p>${employee.phone}</p>
-  <p>${employee.address}</p>
-  <p>${employee.birthday}</p>
-  </div>
-  `;
+  const divInnerHTML = createModalEmployeeInfo(index, employees);
+  modalDiv.appendChild(divInnerHTML.img);
+  modalDiv.appendChild(divInnerHTML.basicInfo);
+  modalDiv.appendChild(divInnerHTML.hr);
+  modalDiv.appendChild(divInnerHTML.extraInfo);
+
+
+  //Add scroll buttons
+  createScrollButton('left', index, modalDiv, employees);
+  createScrollButton('right', index, modalDiv, employees);
 
   //Add div to DOM
   overlay.appendChild(modalDiv);
@@ -117,7 +212,7 @@ const giveEmployeeDivListener = (employee, employeeDiv, overlay) => {
   employeeDiv.addEventListener('click', () => {
     overlay.className = '';
     overlay.style.display = 'flex';
-    createModalPopUp(employee, overlay);
+    createModalPopUp(employee.id, employees, overlay);
   })
 }
 
@@ -165,3 +260,4 @@ fetch(`https://randomuser.me/api/?nat=us&results=${numEmployees}`)
   .then(response => response.json())
   .then(data => data.results)
   .then(results => results.forEach((employee, index) => createEmployeeElements(employee, index, gridDiv, overlay)));
+
