b325bb87a32b6be96601f33c5dcce3035f1ab565
Alexander C Perry <acp96@cornell.edu>
2020-06-13 21:10:40 -0400

    search bar styling

--- js/search.js
+++ js/search.js
@@ -19,21 +19,3 @@ const setupSearch = () => {
     })
   })
 }
-
-// $('.search-box').on('keyup', function () {
-
-//   let searchTerm = $('.search-box').val().toLowerCase();
-
-//   $('.gallery-link').each(function (index, link) {
-
-//     let photoDescription = $(link).attr('data-title').toLowerCase();
-
-//     console.log(!photoDescription.includes(searchTerm));
-//     if (!photoDescription.includes(searchTerm)) {
-//       $(link).css('display', 'none');
-//     } else {
-//       $(link).css('display', 'block');
-//     }
-
-//   });
-// });
